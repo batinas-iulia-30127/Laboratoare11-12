0392b8e5f8b61741958f5accfa76871c2f83f220
Popescu Adina <adina.popescu@student.utcluj.ro>
2020-05-20 23:30:17 +0300

    ex1 : Adina

--- c4-samples/src/main/java/aut/utcluj/isp/ex1/Shop.java
+++ c4-samples/src/main/java/aut/utcluj/isp/ex1/Shop.java
@@ -1,5 +1,7 @@
 package aut.utcluj.isp.ex1;
 
+import java.util.Objects;
+
 /**
  * @author stefan
  */
@@ -8,12 +10,32 @@ public class Shop {
     private String city;
 
     public Shop(String name, String city) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.name = name;
+        this.city = city;
     }
 
     public Shop(String name) {
         this.city = "";
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Shop: " + this.name + " City: " + this.city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Shop shop = (Shop) o;
+        return name.equals(shop.name) &&
+                city.equals(shop.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, city);
     }
 
     public String getName() {

4375a62054de9ae5bdf9fac89f7c28264d215701
Batinas Iulia <iuliabatinas@yahoo.ro>
2020-05-24 21:55:38 +0300

    user

--- c4-samples/src/main/java/aut/utcluj/isp/ex4/User.java
+++ c4-samples/src/main/java/aut/utcluj/isp/ex4/User.java
@@ -4,20 +4,24 @@ package aut.utcluj.isp.ex4;
  * @author stefan
  */
 public class User {
-    private UserCart userCart;
+    private UserCart userCart = new UserCart();
     private Double userMoney;
 
     public User(Double userMoney) {
+
         this.userMoney = userMoney;
     }
 
     public UserCart getUserCart() {
+
         return userCart;
     }
 
     public Double getUserMoney() {
+
         return userMoney;
     }
+
 
     /**
      * Add product to the cart
@@ -26,7 +30,7 @@ public class User {
      * @param quantity - quantity to be added
      */
     public void addProductToCart(Product product, int quantity) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        userCart.addProductToCart(product, quantity);
     }
 
     /**
@@ -35,8 +39,8 @@ public class User {
      *
      * @param productId - unique product id
      */
-    public void removeProductFromCart(final String productId) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void removeProductFromCart(final String productId) throws ProductNotFoundException {
+        userCart.removeProductFromCart(productId);
     }
 
     /**
@@ -45,7 +49,13 @@ public class User {
      * If userMoney is less than total price of the products, an {@link NotEnoughMoneyException} exception will be thrown
      * If userMoney is greater or equal to the total price of the products, total price should be extracted from total money and cart to be reset to default values
      */
-    public void submitCart() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void submitCart() throws NotEnoughMoneyException {
+        if (userMoney <= userCart.getTotalPrice()) {
+            userMoney = userMoney - userCart.getTotalPrice();
+            userCart.resetCart();
+        } else {
+            throw new NotEnoughMoneyException();
+        }
+
     }
 }
